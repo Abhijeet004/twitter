43a859fdb9ca66e92f12c4c6777ec33255f87ad0
Abhijeet004 <17ucc004@lnmiit.ac.in>
2020-10-07 22:27:16 +0530

    changes

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const User = require('../models/user-model');
 const passport = require("passport");
 const CLIENT_HOME_PAGE_URL = "https://second-twit.herokuapp.com/";
 
@@ -39,5 +40,43 @@ router.get(
     failureRedirect: "/auth/login/failed"
   })
 );
+
+
+router.get('/', (req, res) => {
+  var todayDate = new Date();
+  let tdd = todayDate.getDate();
+  let tmm = todayDate.getMonth() + 1;
+  if (tmm < 10) tmm = '0' + tmm;
+  if (tdd < 10) tdd = '0' + tdd;
+  let tyyyy = todayDate.getFullYear();
+
+  var last7Date = new Date();
+  last7Date.setDate(last7Date.getDate() - 7);
+
+  let ldd = last7Date.getDate();
+  let lmm = last7Date.getMonth() + 1;
+  if (lmm < 10) lmm = '0' + lmm;
+  if (ldd < 10) ldd = '0' + ldd;
+  let lyyyy = last7Date.getFullYear();
+
+  client.get(
+    'search/tweets',
+    {
+      q: `a since:${lyyyy + '-' + lmm + '-' + ldd} until:${
+        tyyyy + '-' + tmm + '-' + tdd
+      }`,
+      count: 100,
+    },
+    (error, data, response) => {
+      if (error) res.send(error);
+      const tweets = data;
+      User.remove({});
+      let postTweets = new User({ tweets });
+      postTweets.save();
+
+      res.send(postTweets);
+    }
+  );
+});
 
 module.exports = router;
